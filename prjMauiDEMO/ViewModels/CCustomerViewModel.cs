e615b31fb90c560c610b5ed22ddebc9e11073bee
bian <bute77889@gmail.com>
2023-10-31 13:58:45 +0800

    TODO選列表回主選單

--- prjMauiDEMO/ViewModels/CCustomerViewModel.cs
+++ prjMauiDEMO/ViewModels/CCustomerViewModel.cs
@@ -1,77 +1,77 @@
-﻿using System;
-using System.Collections;
+﻿using prjMAUIDEMO.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using prjMAUIDEMO.Models;
 
 namespace prjMAUIDEMO.ViewModels
 {
-    public class CCustomerViewModel:INotifyPropertyChanged
+    public class CCustomerViewModel
     {
         private List<CCustomer> _list = new List<CCustomer>();
-        private int _position;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public CCustomerViewModel()
         {
-            loadData();
+            LoadData();
         }
+        private int _position = 0;
+
+
         public CCustomer current { get { return _list[_position]; } }
+
         public List<CCustomer> all { get { return _list; } }
 
-        private void loadData()
+
+
+        public void LoadData()
         {
-            _list.Add(new CCustomer() { id = 1, address = "Taipei", email = "john@gmail.com", name = "John", phone = "0955312654" });
-            _list.Add(new CCustomer() { id = 2, address = "Taoyuan", email = "peggy@gmail.com", name = "Peggy", phone = "0934212336" });
-            _list.Add(new CCustomer() { id = 5, address = "Taipei", email = "wman@gmail.com", name = "Wman", phone = "0980556221" });
+            _list.Add(new CCustomer() { id = 1, name = "Tom", phone = "0910147258", email = "tom@gmail.com", address = "Taipei" });
+            _list.Add(new CCustomer() { id = 2, name = "Storm", phone = "0910258369", email = "strom@gmail.com", address = "Keelung" });
+            _list.Add(new CCustomer() { id = 3, name = "Steam", phone = "0910369147", email = "steam@gmail.com", address = "Taoyuan" });
         }
+
         public void moveFirst()
         {
             _position = 0;
-            PropertyChanged(this, new PropertyChangedEventArgs("current"));
+
         }
+
         public void movePrevious()
         {
             _position--;
-            if (_position < 0) _position = 0;
-            PropertyChanged(this, new PropertyChangedEventArgs("current"));
+            if (_position < 0)
+                _position = 0;
         }
         public void moveNext()
         {
             _position++;
-            if (_position > _list.Count - 1) _position = _list.Count - 1;
-            PropertyChanged(this, new PropertyChangedEventArgs("current"));
+            if (_position >= _list.Count)
+                _position = _list.Count - 1;
         }
         public void moveLast()
         {
             _position = _list.Count - 1;
-            PropertyChanged(this, new PropertyChangedEventArgs("current"));
         }
+
         public void moveTo(int to)
         {
             _position = to;
-            PropertyChanged(this, new PropertyChangedEventArgs("current"));
         }
 
         public CCustomer queryByKeyword(string keyword)
         {
             for (int i = 0; i < _list.Count; i++)
             {
-                if (
-                    _list[i].name.ToUpper().Contains(keyword.ToUpper()) ||
-                    _list[i].address.ToUpper().Contains(keyword.ToUpper()) ||
+                if (_list[i].name.ToUpper().Contains(keyword.ToUpper()) ||
+                    _list[i].phone.ToUpper().Contains(keyword.ToUpper()) ||
                     _list[i].email.ToUpper().Contains(keyword.ToUpper()) ||
-                    _list[i].phone.ToUpper().Contains(keyword.ToUpper())
-                    )
+                    _list[i].address.ToUpper().Contains(keyword.ToUpper()))
                 {
                     _position = i;
-                    PropertyChanged(this, new PropertyChangedEventArgs("current"));
                     return _list[i];
                 }
+
+
             }
             return null;
         }
